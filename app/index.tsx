12cedcc91701d82d799e347f3a602a5d1c6e650b
Sahil Suman <sahilsuman1202@gmail.com>
2025-04-18 11:49:44 +0530

    pushing initial files

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-gray-100">
       <Text className="text-5xl text-dark-200
-      font-bold">Welcome</Text>
+      font-bold">Welcome</Text> 
 
 
     </View>
