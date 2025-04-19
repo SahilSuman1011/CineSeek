221cf75dac77b8abc5a83c5c1cc90b31915e4950
Sahil Suman <sahilsuman1202@gmail.com>
2025-04-19 11:53:10 +0530

    app logo added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,10 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import {Link} from "expo-router";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-gray-100">
-      <Text className="text-5xl text-blue-500 font-bold">CineSeek</Text>
-
+    <View className="flex-1 bg-primary">
+     <Image/>
     </View>
   );
 }
